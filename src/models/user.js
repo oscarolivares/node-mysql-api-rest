e47622174ee299a82f3fe1366031b4498028794e
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-21 21:45:18 -0400

    Se declara el modelo con sus metodos para listar y crear usuarios, tambien se definen las respectivas rutas de la api

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,38 @@
 const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '2660882',
+  database: 'mysqlapi_test'
+});
+
+let userModel = {};
+
+userModel.getUsers = callback => {
+  if (connection) {
+    connection.query('SELECT * FROM users ORDER BY id', (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        callback(null, result);
+      }
+    });
+  }
+};
+
+userModel.createUser = (userData, callback) => {
+  if (connection) {
+    connection.query('INSERT INTO users SET ?', userData, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        callback(null, {
+          insertId: result.insertId
+        });
+      }
+    });
+  }
+};
+
+module.exports = userModel;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,27 @@
+const User = require('../models/user');
+
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.json([]);
+  app.get('/users', (req, res) => {
+    User.getUsers((err, result) => {
+      res.status(200).json(result);
+    });
+  });
+
+  app.post('/users', (req, res) => {
+    const userData = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    User.createUser(userData, (err, result) => {
+      if (result && result.insertId) {
+        res.status(200).json({
+          success: true,
+          msg: 'User added',
+          data: result
+        });
+      }
+    });
   });
 };
